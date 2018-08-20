4ac4269d9514f7502f17bd54f1d611859e7c2ebc
Larry Sprock <larry@lucidbleu.com>
2018-08-20 10:52:34 -0700

    Add initial imports

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 'use strict';
+var fastbootTransform = require('fastboot-transform');
 
 module.exports = {
-  name: 'branch-cli-sdk'
+  name: 'branch-cli-sdk',
+
+  options: {
+    nodeAssets: {
+      'branch-sdk': {
+        srcDir: 'dist',
+        import: {
+          include: [
+            'build.js'
+          ],
+          processTree(input) {
+            return fastbootTransform(input);
+          }
+        }
+      }
+    }
+  }
 };
