2a0c1d7c72bd9833f007913a43042a36ce37a779
Larry Sprock <larry@lucidbleu.com>
2019-01-23 13:03:29 -0800

    Refactor fastboot transform

--- index.js
+++ index.js
@@ -2,25 +2,51 @@
 
 var path = require('path');
 var fastbootTransform = require('fastboot-transform');
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+const resolve = require('resolve');
 
 module.exports = {
-  name: 'ember-cli-branch',
+  name: require('./package').name,
 
-  included: function (app) {
+  included: function () {
     this._super.included.apply(this, arguments);
-    var branchPath = path.join('node_modules', 'branch-sdk', 'dist', 'build.js');
-    var importOptions = {
-      using: [{
-        transformation: 'fastbootTransform'
-      }]
-    };
+    this._ensureFindHost();
 
-    app.import(branchPath, importOptions);
+    let vendorPath = `vendor/${this.name}`;
+    let host = this._findHost();
+
+    host.import(path.join(vendorPath, 'build.js'));
   },
 
-  importTransforms: function () {
-    return {
-      fastbootTransform: fastbootTransform
+  treeForVendor() {
+    let branchPath = path.join(this.resolvePackagePath('branch-sdk'), 'dist');
+
+    let branchJs = fastbootTransform(new Funnel(branchPath, {
+      files: ['build.js'],
+      destDir: this.name
+    }));
+
+    return mergeTrees([branchJs]);
+  },
+
+  resolvePackagePath(packageName) {
+    let host = this._findHost();
+    return path.dirname(resolve.sync(`${packageName}/package.json`, { basedir: host.project.root }));
+  },
+
+  _ensureFindHost() {
+    if (!this._findHost) {
+      this._findHost = function findHostShim() {
+        let current = this;
+        let app;
+
+        do {
+          app = current.app || app;
+        } while (current.parent.parent && (current = current.parent));
+
+        return app;
+      };
     }
   }
 };
